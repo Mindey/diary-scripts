6998f9f3be45ea7973dda862f2c161cdd232e94b
Mindey <mindey@qq.fake>
2013-10-04 12:56:54 -0400

    Corrected the generate-days.py by changing symbol of (■, ● ) geneartion, so that it would work correctly with the getdays.py.

--- generate-days.py
+++ generate-days.py
@@ -4,7 +4,7 @@ from sys import argv
 import datetime
 
 if len(argv) < 3:
-    print "For example, to generate list of 1000 days starting 1999-09-09, type: python generate-days.py 1999-09-09 1000"
+    print "For example, to generate list of 1000 days starting 1999-09-09, type: python generate-days.py 1999-09-09 20 > sample.txt"
 else:
     y,m,d = [int(i) for i in argv[1].split('-')]
     # Get the number of days:
@@ -14,5 +14,5 @@ else:
     # Go through the next n days:
     for i in range(n):
         day = d+datetime.timedelta(days=i)
-        weekday = lambda x: x!=7 and x or '■'
-        print "%5s ⬛ " % (str(i+1),), str(day), "["+str(weekday(day.weekday()+1))+"] "
+        weekday = lambda x: x!=7 and x or '●'
+        print "%5s ■ " % (str(i+1),), str(day), "["+str(weekday(day.weekday()+1))+"] "
